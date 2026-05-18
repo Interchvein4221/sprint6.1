290849784242a142eed9e8e87a047ff24d155634
A.Akhmarov <artur.ahmaroff@yandex.ru>
2026-05-18 22:35:38 +0500

    31

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"log"
 	"os"
+
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
-	
+
 func main() {
 
 	logger := log.New(os.Stdout, "INFO\t", log.LstdFlags)
 	srv := server.NewServer(logger)
-	err := srv.HTTP.ListenAndServe()
-	if err != nil {
+	if err := srv.HTTP.ListenAndServe(); err != nil {
 		logger.Fatal(err)
 	}
 
